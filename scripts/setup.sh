3d25d44a0633c3ea3cc807296d4a410dab58a9c3
Lorent Lempereur <lorent@commuty.net>
2019-10-22 12:40:28 +0200

    Upgrade services

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,5 +3,5 @@ docker-compose build --pull
 docker-compose up -d postgres
 sleep 2
 
-docker-compose run --rm engine bundle exec rake db:create db:migrate
+docker-compose run --rm engine bundle exec rake db:create db:schema:load
 docker-compose run --rm event-store bundle exec rake db:create db:migrate
